0b4d776ec1a362a2d00248ab91b4c909b2ce1691
Otaiki1 <otaikisadiq2804@gmail.com>
2022-10-29 21:10:31 +0100

    refac: fix responsiveness issues; make navbar static

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -4,7 +4,7 @@ import Logo from '../assets/Logo.svg';
 export default function Navbar(){
 
     return(
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div class="container">
                 <a class="navbar-brand" href="#">
                     <img src={Logo} alt="logo" />
@@ -15,10 +15,10 @@ export default function Navbar(){
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item me-5">
+                    <li class="nav-item me-md-5 mt-3 mt-md-0">
                         <a class="nav-link btn btn-dark text-white" href="#">Find a Job</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mt-3 mt-md-0">
                         <a class="nav-link btn btn-dark text-white" href="#">Post a Job</a>
                     </li>
                    
