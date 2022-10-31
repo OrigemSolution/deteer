5e1425479045ab71e4c91500aab86b8f326ffef1
Otaiki1 <otaikisadiq2804@gmail.com>
2022-10-31 10:21:02 +0100

    feat: add layout  for main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+
+
+export default function Main(){
+
+    return(
+       <div className="container">
+        <div className="row py-4 vh-100">
+            <div className="col-4 bg-white h-100,"></div>
+            <div className="col-6 bg-white h-100"></div>
+            <div className="col-2 h-50"></div>
+        </div>
+       </div>
+    )
+}
